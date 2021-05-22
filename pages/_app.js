91ffb1f7e69b56151569b2d37387abcfd0184f20
hiskio-daniel <daniel.yeh@hiskio.com>
2021-05-22 18:52:00 +0800

    added export

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,13 @@
 import "../styles/globals.css";
-import store from '../redux/store'
-import { Provider } from 'react-redux'
+import store from "../redux/store";
+import { Provider } from "react-redux";
 
 function MyApp({ Component, pageProps }) {
   return (
-  <Provider store={store}>
-    <Component {...pageProps} />;
-  </Provider>
-  )
-  
+    <Provider store={store}>
+      <Component {...pageProps} />;
+    </Provider>
+  );
 }
 
 export default MyApp;
