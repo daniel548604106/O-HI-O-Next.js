b6d2e306ef8eaa650c1e835e8a786e4209721fd4
hiskio-daniel <daniel.yeh@hiskio.com>
2021-05-23 16:30:02 +0800

    update mongodb

--- utils/mongoDB.js
+++ utils/mongoDB.js
@@ -1,6 +1,30 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI;
+// const MONGO_URI = process.env.MONGO_URI;
+
+// if (!MONGO_URI) {
+//   throw new Error(
+//     "Please define the MONGODB_URI environment variable inside .env.local"
+//   );
+// }
+
+// async function dbConnect() {
+//   if (mongoose.connection.readyState >= 1) return;
+//   return mongoose.connect(MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: true,
+//   });
+// }
+
+// mongoose.disconnect();
+
+// export default dbConnect;
+
+import { MongoClient } from "mongodb";
+
+const { MONGO_URI } = process.env;
 
 if (!MONGO_URI) {
   throw new Error(
@@ -8,16 +32,35 @@ if (!MONGO_URI) {
   );
 }
 
-async function dbConnect() {
-  if (mongoose.connection.readyState >= 1) return;
-  return mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: true,
-  });
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongo;
+
+if (!cached) {
+  cached = global.mongo = { conn: null, promise: null };
 }
 
-mongoose.disconnect();
+export default async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
 
-export default dbConnect;
+  if (!cached.promise) {
+    const opts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+
+    cached.promise = MongoClient.connect(MONGO_URI, opts).then((client) => {
+      return {
+        client,
+        db: client.db(MONGODB_DB),
+      };
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
