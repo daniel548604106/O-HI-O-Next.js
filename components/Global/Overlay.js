cac3528c30c130cba039767d80ea987da1d9c2d1
hiskio-daniel <daniel.yeh@hiskio.com>
2021-05-28 19:18:12 +0800

    modify sidemenu and fix mongodb

--- components/Global/Overlay.js
+++ components/Global/Overlay.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Overlay = ({ children }) => {
   return (
-    <div className="bg-gray-600 opacity-50 fixed top-0 left-0 w-screen h-screen">
+    <div className="bg-black z-50 bg-opacity-50 fixed top-0 left-0 w-screen h-screen">
       {children}
     </div>
   );
