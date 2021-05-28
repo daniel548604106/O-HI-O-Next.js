63a9bb557a76fc5544d10c2fee46b000343ac9b9
hiskio-daniel <daniel.yeh@hiskio.com>
2021-05-28 19:35:31 +0800

    modify head

--- pages/_app.js
+++ pages/_app.js
@@ -5,19 +5,25 @@ import { Provider } from "react-redux";
 import { useRouter } from "next/router";
 import Footer from "../components/Global/Footer";
 import Header from "../components/Global/Header";
+import Head from "next/head";
 import SideMenu from "../components/Global/SideMenu";
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   return (
-    <Provider store={store}>
-      <div className={`${router.pathname === "/application" && "hidden"}`}>
-        <Header />
-      </div>
-      <SideMenu />
-      <Component {...pageProps} />;
-      <Footer />
-    </Provider>
+    <>
+      <Head>
+        <title>O.HI.O</title>
+      </Head>
+      <Provider store={store}>
+        <div className={`${router.pathname === "/application" && "hidden"}`}>
+          <Header />
+        </div>
+        <SideMenu />
+        <Component {...pageProps} />;
+        <Footer />
+      </Provider>
+    </>
   );
 }
 
